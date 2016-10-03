0e3ad8790e1103aff85cc6431be1bc2afca465c2
Pedro Henrique <lppedrohs@gmail.com>
2016-10-03 19:56:53 -0300

    Adicionando relação dinamica ao modelo de parametro

--- lib/generators/jera_push/jera_push_generator.rb
+++ lib/generators/jera_push/jera_push_generator.rb
@@ -2,11 +2,30 @@ class JeraPushGenerator < Rails::Generators::NamedBase
   desc "This generator creates an initializer file at config/initializers"
   source_root File.expand_path("../templates", __FILE__)
 
+  MissingModel = Class.new(Thor::Error)
+
   def copy_initializer_file
     copy_file "jera_push.rb", "config/initializers/jera_push.rb"
   end
 
   def generate_migrations
+		unless model_exists?
+			raise MissingModel,
+				"\n\tModel \"#{file_name.titlecase}\" doesn't exists. Please, create your Model and try again."
+		end
+
+		inject_into_file model_path, "\n\thas_many :devices, class_name: 'JeraPush::Device'", after: ' < ActiveRecord::Base'
+
     generate "active_record:jera_push", file_name
   end
+
+  private
+
+  	def model_exists?
+  		File.exist?(File.join(destination_root, model_path))
+  	end
+
+	  def model_path
+		  @model_path ||= File.join("app", "models", "#{file_path}.rb")
+		end
 end
