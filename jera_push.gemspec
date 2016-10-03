3ebee49789715604a9d776ae6728c10d11cd2983
Marco Souza <marcoasouza.1@gmail.com>
2016-10-03 10:59:13 -0400

    enumerize

--- jera_push.gemspec
+++ jera_push.gemspec
@@ -18,6 +18,7 @@ Gem::Specification.new do |s|
   s.test_files = Dir["test/**/*"]
 
   s.add_dependency "rails", "~> 4.2.6"
-
+  s.add_dependency "enumerize"
+  
   s.add_development_dependency "sqlite3"
 end
--- lib/jera_push/models/device.rb
+++ lib/jera_push/models/device.rb
@@ -1,6 +1,11 @@
 class JeraPush::Device < ActiveRecord::Base
+  extend Enumerize
+
   self.table_name = "jera_push_devices"
+
   has_many :messages, through: :message_devices
 
   validates :token, :platform, presence: true
+
+  enumerize :platform, in: [:android, :ios, :chrome]
 end
