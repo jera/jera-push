d8f2fa6a79824f1ec5c747e39c898f8adb3f66f9
Marco Souza <marcoasouza.1@gmail.com>
2016-10-04 18:36:45 -0400

    Metodos para modelo de Device

--- lib/jera_push/models/device.rb
+++ lib/jera_push/models/device.rb
@@ -9,5 +9,23 @@ class JeraPush::Device < ActiveRecord::Base
 
   validates :token, :platform, presence: true
 
-  enumerize :platform, in: [:android, :ios, :chrome]
+  enumerize :platform, in: [:android, :ios, :chrome], predicate: true
+
+  scope :ios,  -> { where(platform: :ios) }
+  scope :android, -> { where(platform: :android) }
+  scope :chrome, -> { where(platform: :chrome) }
+
+  def send_message(message)
+    JeraPush::Message.send message: message, devices: [ self ]
+  end
+
+  def subscribe(topic)
+    client = JeraPush::Firebase::Client.instance
+    client.add_device_to_topic(device: self, topic: topic)
+  end
+
+  def unsubscribe(topic)
+    client = JeraPush::Firebase::Client.instance
+    client.remove_device_from_topic(device: [self], topic: topic)
+  end
 end
