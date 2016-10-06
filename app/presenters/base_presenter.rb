7e81a1698f552a58f8c87f8ea4e0dac1e2681ed6
Pedro Henrique <lppedrohs@gmail.com>
2016-10-06 17:12:48 -0300

    Adicionando helper no presenter

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,13 +1,19 @@
 class BasePresenter < SimpleDelegator
   attr_reader :item
 
-  def initialize(item, *args)
-    @item = item
-    __setobj__(item)
-  end
+  protected
 
-  def self.wrap(collection)
-    collection.all.collect { |model| self.new(model) }
-  end
+	  def initialize(item, *args)
+	    @item = item
+	    __setobj__(item)
+	  end
+
+	  def self.wrap(collection)
+	    collection.all.collect { |model| self.new(model) }
+	  end
+
+	  def helpers
+	  	ApplicationController.helpers
+	  end
 
 end
--- app/presenters/jera_push/message_presenter.rb
+++ app/presenters/jera_push/message_presenter.rb
@@ -1,5 +1,4 @@
 class JeraPush::MessagePresenter < BasePresenter
-	include ActionView::Helpers::UrlHelper
 
 	def display_content
 		"\{ title: \'#{item.content["title"]}\' , body: \'#{item.content["body"]}\' \}".html_safe
@@ -10,8 +9,8 @@ class JeraPush::MessagePresenter < BasePresenter
 	end
 
 	def show_link
-		link_to(
-			I18n.t('jera_push.admin.buttons.details'),
+		helpers.link_to(
+			helpers.t('jera_push.admin.buttons.details'),
 			Rails.application.routes.url_helpers.jera_push_admin_message_path(item), class: 'waves-effect waves-light btn'
 		)
 	end
