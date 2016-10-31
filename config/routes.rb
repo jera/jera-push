d41f408a666bc30bbd2ea99a2d0a6f6453c6fd90
Pedro Henrique <lppedrohs@gmail.com>
2016-10-31 10:49:08 -0200

    Adicionando rota index para admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :jera_push do
     namespace :admin do
+
+      get '/', to: 'devices#index'
+
       resources :messages, only: [:index, :show, :new, :create] do
         collection do
           get :device_filter, format: :js
