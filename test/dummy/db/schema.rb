6c6cdfda4abb7deea57ab3e69fe30064d1606f64
Pedro Henrique <lppedrohs@gmail.com>
2016-11-23 19:18:19 -0200

    Dummy

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123185356) do
+ActiveRecord::Schema.define(version: 20161123200421) do
 
   create_table "jera_push_devices", force: :cascade do |t|
     t.string   "token"
