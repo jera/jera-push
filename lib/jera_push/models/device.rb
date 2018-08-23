2bc246c7789e69ff22c205831b22add786dfd1ca
Dimitrius Fernandes <dimitrius@jera.com.br>
2018-08-23 09:17:13 -0400

    solving ActiveRecord::HasManyThroughOrderError

--- lib/jera_push/models/device.rb
+++ lib/jera_push/models/device.rb
@@ -7,8 +7,9 @@ class JeraPush::Device < ActiveRecord::Base
 
   self.table_name = "jera_push_devices"
 
-  has_many :messages, through: :message_devices, table_name: "jera_push_messages"
   has_many :message_devices, table_name: "jera_push_message_devices"
+  has_many :messages, through: :message_devices, table_name: "jera_push_messages"
+
   belongs_to :pushable, polymorphic: true
 
   validates :token, :platform, presence: true
